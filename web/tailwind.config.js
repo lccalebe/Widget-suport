f8f2d5dc9788364f500b45c5fc55b2642919210b
Lucas <lukscalebe@hotmail.com>
2022-05-24 15:12:20 -0300

    update

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,17 @@
+const { borderRadius } = require('tailwindcss/defaulttheme');
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors :{
         brand :{
+          300: '#996DFF',
           500:'#8257e6',
         }
+      },
+      borderRadius :{
+        md: '4px'
       }
     },
   },
